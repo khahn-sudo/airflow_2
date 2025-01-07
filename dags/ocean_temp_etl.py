ca32634e503a219ce21f90c1f6ab89d97feca503
khahn <khahn@itcen.com>
2025-01-07 16:27:59 +0900

    1

--- dags/ocean_temp_etl.py
+++ dags/ocean_temp_etl.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # MySQL에서 데이터 읽는 함수
 def read_ocean_table():
     # MySQL Hook을 사용하여 연결
-    mysql_hook = MySqlHook(mysql_conn_id='mysql_ocean_connection')  # 연결 ID는 설정한 연결 이름
+    mysql_hook = MySqlHook(mysql_conn_id='ocean_admin')  # 연결 ID는 설정한 연결 이름
     query = "SELECT * FROM ocean_admin.ocean"  # 쿼리 작성
     
     # 쿼리 실행 및 결과 가져오기
@@ -23,7 +23,7 @@ def read_ocean_table():
 
 # DAG 정의
 with DAG(
-    'mysql_ocean_read',
+    'ocean_temp_etl',
     default_args={
         'owner': 'airflow',
         'start_date': datetime(2025, 1, 7),
