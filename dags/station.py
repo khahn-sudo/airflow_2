8891dcd132a0614d69459525457e31adf9b4cc44
khahn <khahn@itcen.com>
2025-01-08 13:35:49 +0900

    1

--- dags/station.py
+++ dags/station.py
@@ -21,7 +21,7 @@ with DAG(
 
     fetch_api_data = SimpleHttpOperator(
         task_id='fetch_api_data',
-        endpoint='http://www.khoa.go.kr/api/oceangrid/ObsServiceObj/search.do?ServiceKey=wldhxng34hkddbsgm81lwldhxng34hkddbsgm81l==&ResultType=xml',
+        endpoint='http://www.khoa.go.kr/api/oceangrid/ObsServiceObj/search.do?ServiceKey=wldhxng34hkddbsgm81lwldhxng34hkddbsgm81l==&ResultType=json',
         method='GET',
         response_filter=lambda response: response.text,
         log_response=True,
